c7a4f2aab3e4eaa47a825ff085135ea339ec6039
Aléxis Oliveira <alexis.silvaoliveira@gmail.com>
2022-08-27 09:33:02 -0300

    Creating number buttons eventlistener

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let result = document.getElementById('result').lastElementChild;
 const AC = document.getElementById("AC");
 const DEL = document.getElementById("DEL");
 
+const buttons = document.getElementsByClassName("number");
+
 AC.addEventListener("click", allClear);
 
 DEL.addEventListener("click", () => {
@@ -17,6 +19,24 @@ DEL.addEventListener("click", () => {
     }
 
 });
+
+for (const key in buttons) {
+    if (Object.hasOwnProperty.call(buttons, key)) {
+        const element = buttons[key];
+        element.addEventListener("click", (e) =>{
+            if(calcHistory.textContent.includes("=")) {
+                allClear();
+                result.innerText = e.target.textContent;
+            } 
+            else if(result.textContent.length == 1 && result.textContent == "0") {
+                result.innerText = e.target.textContent;
+            }
+            else{
+                result.innerText += e.target.textContent;
+            }
+        })
+    }
+}
 
 function allClear(){
     calcHistory.innerText = "0";
